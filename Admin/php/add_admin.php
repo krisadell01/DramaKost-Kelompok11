43ed46c8d19e3c39287145954c0ae461b6cc616e
Krisadell <90303179+krisadell01@users.noreply.github.com>
2021-12-10 21:46:57 +0700

    membuat add_admin.php
    php sign up

---
+++ Admin/php/add_admin.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['admin']))
+{
+  header("Location: http://localhost/DRAMAKOST/login.php");
+}
+
+    $db=mysqli_connect("localhost", "Krisadell", "Kr1s4nd1_03", "dramakost");
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $pass = mysqli_real_escape_string($db, $_POST['password']);
+    $pass = hash('sha512', $pass);
+    $query = "INSERT INTO admin (Username, Email, Password) VALUES ('$username', '$email', '$pass')";
+
+    $result = mysqli_query($db, $query);
+
+    header("Location: http://localhost/DRAMAKOST/admin/insert_post.html");
+
+?>
