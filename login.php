34212ec241dc83f79c04da5d0701559251921968
Krisadell <90303179+krisadell01@users.noreply.github.com>
2021-12-11 21:24:42 +0700

    Update login.php

--- login.php
+++ login.php
@@ -19,13 +19,13 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class="nav-link font-weight-bold" href="index.html">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link font-weight-bold" href="http://localhost/DRAMAKOST/index.php">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
           <a class="nav-link font-weight-bold" href="about.html">About Us</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link font-weight-bold" href=contact.html#">Contact Us</a>
+          <a class="nav-link font-weight-bold" href="contact.html">Contact Us</a>
         </li>
         <li class="nav-item">
           <a class="nav-link font-weight-bold" href="#">Privacy Policy</a>
